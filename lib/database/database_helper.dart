0b811e23bb998654309b4506ee506116692fc7ca
Farhiya <fafrah003@gmail.com>
2026-03-05 11:03:47 -0500

    Made SQLite database and added 2 suits with the cards

--- lib/database/database_helper.dart
+++ lib/database/database_helper.dart
@@ -1,16 +1,19 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
 class DatabaseHelper {
   static final DatabaseHelper instance = DatabaseHelper._init();
   static Database? _database;
 
   DatabaseHelper._init();
 
-  Future get database async {
+  Future<Database> get database async {
     if (_database != null) return _database!;
-    _database = await _initDB('card_organizer.db');
+    _database = await _initDB('cards.db');
     return _database!;
   }
 
-  Future _initDB(String filePath) async {
+  Future<Database> _initDB(String filePath) async {
     final dbPath = await getDatabasesPath();
     final path = join(dbPath, filePath);
 
@@ -18,48 +21,44 @@ class DatabaseHelper {
   }
 
   Future _createDB(Database db, int version) async {
-    // Create Folders table
     await db.execute('''
       CREATE TABLE folders(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        folder_name TEXT NOT NULL,
-        timestamp TEXT NOT NULL
+        folder_name TEXT,
+        timestamp TEXT
       )
     ''');
 
-    // Create Cards table with foreign key
     await db.execute('''
       CREATE TABLE cards(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        card_name TEXT NOT NULL,
-        suit TEXT NOT NULL,
+        card_name TEXT,
+        suit TEXT,
         image_url TEXT,
         folder_id INTEGER,
-        FOREIGN KEY (folder_id) REFERENCES folders (id)
-          ON DELETE CASCADE
+        FOREIGN KEY(folder_id) REFERENCES folders(id) ON DELETE CASCADE
       )
     ''');
 
-    // Prepopulate folders
-    await _prepopulateFolders(db);
-
-    // Prepopulate cards
-    await _prepopulateCards(db);
+    await _insertFolders(db);
+    await _insertCards(db);
   }
 
-  Future _prepopulateFolders(Database db) async {
-    final folders = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
-    for (int i = 0; i < folders.length; i++) {
+  Future _insertFolders(Database db) async {
+    List suits = ['Hearts', 'Spades'];
+
+    for (var suit in suits) {
       await db.insert('folders', {
-        'folder_name': folders[i],
-        'timestamp': DateTime.now().toIso8601String(),
+        'folder_name': suit,
+        'timestamp': DateTime.now().toString(),
       });
     }
   }
 
-  Future _prepopulateCards(Database db) async {
-    final suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
-    final cards = [
+  Future _insertCards(Database db) async {
+    List suits = ['Hearts', 'Spades'];
+
+    List cards = [
       'Ace',
       '2',
       '3',
@@ -80,8 +79,7 @@ class DatabaseHelper {
         await db.insert('cards', {
           'card_name': card,
           'suit': suits[folderId - 1],
-          'image_url':
-              'assets/cards/${suits[folderId - 1].toLowerCase()}_$card.png',
+          'image_url': '',
           'folder_id': folderId,
         });
       }
