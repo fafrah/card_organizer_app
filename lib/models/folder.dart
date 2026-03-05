4f55c7b40417e0d77b2b9b69ff81ac6d78b25c17
Farhiya <fafrah003@gmail.com>
2026-03-05 12:01:06 -0500

    Added the folder and playing card models

---
+++ lib/models/folder.dart
@@ -0,0 +1,19 @@
+class Folder {
+  final int? id;
+  final String folderName;
+  final String timestamp;
+
+  Folder({this.id, required this.folderName, required this.timestamp});
+
+  Map<String, dynamic> toMap() {
+    return {'id': id, 'folder_name': folderName, 'timestamp': timestamp};
+  }
+
+  factory Folder.fromMap(Map map) {
+    return Folder(
+      id: map['id'],
+      folderName: map['folder_name'],
+      timestamp: map['timestamp'],
+    );
+  }
+}
